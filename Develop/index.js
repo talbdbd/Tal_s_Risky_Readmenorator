9caf4c82b50690aa4a2a67260d2306bb2d127ce0
talbdbd <bendavid.bd.tal@gmail.com>
2024-06-21 22:56:28 -0700

    fixed generateMarkdown pathway in index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('Develop/utils/generateMarkdown.js');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 const questions = [
   {
